a1a92d3c19f84adc030a143362ace309cc25e3f9
Connor Coale <connorcoale@gmail.com>
2026-01-01 23:10:52 -0500

    added reg instantation and signal renaming as practice

--- src/part_03/lib/solve_03.ml
+++ src/part_03/lib/solve_03.ml
@@ -21,6 +21,13 @@ end
 
 let circuit scope (i : _ I.t) = 
 
+  let spec =
+    Reg_spec.create
+      ~clock:i.clock
+      ~reset:i.reset
+      () 
+  in
+
   let helper_in = 
     {
     Helper_03.I.in1 = i.cs;
@@ -29,9 +36,22 @@ let circuit scope (i : _ I.t) =
   in
 
   let helper = Helper_03.hierarchical scope helper_in in
+  let out1_q = reg spec helper.out1 in
+  let out1_q32 = 
+    let r = reg spec (uresize out1_q 32) in
+    Scope.naming scope r "out1_q32"
+  in
 
-  let valid32 = repeat helper.out1 32 in
-  let data_qual = valid32 &: uresize i.data 32 in
+  let shift_regs : Signal.t array = Array.make 4 (zero 4) in
+  for stage = 0 to 3 do
+    let input = if stage = 0 then uresize out1_q 4 else shift_regs.(stage - 1) in
+    shift_regs.(stage) <- reg spec input;
+    ignore (Scope.naming scope shift_regs.(stage) ("shift_" ^ string_of_int stage))
+  done;
+
+  let shift_3 = uresize (shift_regs.(3)) 32 in
+  let data_qual = out1_q32 &: uresize i.data 32 &: shift_3 in
+
 
   { O.solution_a = data_qual;
     solution_b = data_qual;
